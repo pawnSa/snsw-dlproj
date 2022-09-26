1ba0f15b4158924491a296046f35c33a9b4daa12
jadepate <linnetjade@gmail.com>
2022-09-26 16:25:25 +1000

    Added following files: application.conf logback.xml
    Updated following files:
    Application.kt
    build.gradle.kts
    
    User must make sure they import files before continuing

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,5 +1,13 @@
-fun main () {
-    println("Hello Team")
-    println("This is a Group F project")
-    println("New addition")
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.init() {
+    routing {
+        get("/") {
+            call.respondText("Hello, World!")
+        }
+    }
 }
