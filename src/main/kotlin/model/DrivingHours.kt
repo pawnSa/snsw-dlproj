9ca95f67f24363128ba4fbcc14ffef6913480654
Your Name <you@example.com>
2022-10-04 14:14:20 +1100

    Update travelTime

--- src/main/kotlin/model/DrivingHours.kt
+++ src/main/kotlin/model/DrivingHours.kt
@@ -10,5 +10,6 @@ data class DrivingHours (
     val date: String,
     val startTime: String,
     val endTime: String,
+    val travelTime: Double,
     @Serializable(with = ObjectIdAsStringSerializer::class) val _id: Id<DrivingHours> = newId()
         )
